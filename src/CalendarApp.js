27f296862439c05bb8059bd4c4e61bb747a16f4e
Max <maxrogerssepulveda@gmail.com>
2020-10-19 19:58:20 -0300

    Components LoginScreen, CalendarScreen, CalendarApp & AppRouter added

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,9 +1,10 @@
 import React from "react";
+import AppRouter from "./router/AppRouter";
 
 const CalendarApp = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <AppRouter />
     </div>
   );
 };
