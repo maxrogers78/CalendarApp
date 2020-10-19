bc29e8c4d226da13cba6e5bda42b1cfe3c1c5143
Max <maxrogerssepulveda@gmail.com>
2020-10-19 20:16:21 -0300

    Component CalendarScreen modified

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,8 +1,12 @@
+// React
 import React from "react";
+// Components
+import NavBar from "../ui/NavBar";
 
 const CalendarScreen = () => {
   return (
     <div>
+      <NavBar />
       <h1>Calendar Screen</h1>
     </div>
   );
