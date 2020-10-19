b74a7f489b197fc7c316709997e164bc812eecfc
Max <maxrogerssepulveda@gmail.com>
2020-10-19 19:50:27 -0300

    Component CalendarApp added

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 // Bootswatch
 import "bootswatch/dist/lux/bootstrap.min.css";
+// Components
+import CalendarApp from "./CalendarApp";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<CalendarApp />, document.getElementById("root"));
