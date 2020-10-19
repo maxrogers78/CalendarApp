efabf63a25951af54279c74f7514248ef3e59c12
Max <maxrogerssepulveda@gmail.com>
2020-10-19 20:16:04 -0300

    Component AppRouter added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,10 +1,27 @@
+// React
 import React from "react";
+// Router
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+// Components
+import LoginScreen from "../components/auth/LoginScreen";
+import CalendarScreen from "../components/calendar/CalendarScreen";
 
 const AppRouter = () => {
   return (
-    <div>
-      <h1>App Router</h1>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/" component={CalendarScreen} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
