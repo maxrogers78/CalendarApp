1ed9cfe4d04d459de317623494f5cba28c0803c3
Max <maxrogerssepulveda@gmail.com>
2020-10-19 20:35:23 -0300

    calendar template added with basic config

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,13 +1,35 @@
 // React
 import React from "react";
+// BigCalendar
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+// MomentJs
+import moment from "moment";
 // Components
 import NavBar from "../ui/NavBar";
 
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Cumple del jefe",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+  },
+];
+
 const CalendarScreen = () => {
   return (
-    <div>
+    <div className="calendar-screen">
       <NavBar />
-      <h1>Calendar Screen</h1>
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   );
 };
