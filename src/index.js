25ede00a2cbfb7fd853eabcae566511031707067
Max <maxrogerssepulveda@gmail.com>
2020-10-20 20:27:33 -0300

    Changed bootwatch library from LUX to JOURNAL

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // Bootswatch
-import "bootswatch/dist/lux/bootstrap.min.css";
+import "bootswatch/dist/journal/bootstrap.min.css";
 // CSS
 import "./styles.css";
 // Components
