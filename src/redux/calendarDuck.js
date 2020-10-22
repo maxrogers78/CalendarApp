9614948d13c1abe49544a72b714eb44ccdf03e42
Max <maxrogerssepulveda@gmail.com>
2020-10-22 19:55:26 -0300

    eventSetActive & eventAddNew actions & types added

--- src/redux/calendarDuck.js
+++ src/redux/calendarDuck.js
@@ -1,14 +1,36 @@
-// constants
-const initialState = {
+// MomentJs
+import moment from "moment";
+
+//! constants
+
+const uiInitialState = {
   modalOpen: false,
 };
+const calendarInitialState = {
+  events: [
+    {
+      title: "Cumple del jefe",
+      start: moment().toDate(),
+      end: moment().add(2, "hours").toDate(),
+      bgcolor: "#fafafa",
+      user: {
+        _id: "123",
+        name: "Maximiliano",
+      },
+    },
+  ],
+  activeEvent: null,
+};
 
-// types
+//! types
 const uiOpenModal = "[ui] Open modal";
 const uiCloseModal = "[ui] Close modal";
+const eventSetActive = "[event] Set Active";
+const eventAddNew = "[event] Add new";
 
-// reducer
-export default function uiReducer(state = initialState, action) {
+//! reducers
+// open & close the modal
+function uiReducer(state = uiInitialState, action) {
   switch (action.type) {
     case uiOpenModal:
       return {
@@ -27,7 +49,20 @@ export default function uiReducer(state = initialState, action) {
   }
 }
 
-// actions
+// add & set active an event
+function calendarReducer(state = calendarInitialState, action) {
+  switch (action.type) {
+    case eventSetActive:
+      return { ...state, activeEvent: action.payload };
+
+    default:
+      return state;
+  }
+}
+
+export default { uiReducer, calendarReducer };
+
+//! actions
 export const uiOpenModalAction = () => (dispatch, getState) => {
   dispatch({
     type: uiOpenModal,
@@ -37,5 +72,19 @@ export const uiOpenModalAction = () => (dispatch, getState) => {
 export const uiCloseModalAction = () => (dispatch, getState) => {
   dispatch({
     type: uiCloseModal,
+  });
+};
+
+export const eventAddNewAction = (event) => (dispatch, getState) => {
+  dispatch({
+    type: eventAddNew,
+    payload: event,
+  });
+};
+
+export const eventSetActiveAction = (event) => (dispatch, getState) => {
+  dispatch({
+    type: eventSetActive,
+    payload: event,
   });
 };
