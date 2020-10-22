aa031f42ac86bce2aaf5e6a6618933aaf59949e2
Max <maxrogerssepulveda@gmail.com>
2020-10-22 20:04:12 -0300

    Add button created & styled

---
+++ src/components/ui/AddNewFab.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { uiOpenModalAction } from "../../redux/calendarDuck";
+
+const AddNewFab = () => {
+  const dispatch = useDispatch();
+
+  const handleClickNew = () => {
+    dispatch(uiOpenModalAction());
+  };
+
+  return (
+    <button className="btn btn-primary fab" onClick={handleClickNew}>
+      <i className="fas fa-plus"></i>
+    </button>
+  );
+};
+
+export default AddNewFab;
